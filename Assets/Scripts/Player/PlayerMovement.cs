839a023768493049f10419059b909469bc5bdca2
AndreaTerenz <andrea.terenziani,at@gmail.com>
2021-04-09 01:31:23 +0200

    Added sprinting to controller

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,23 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 12f;
+    public float sprintSpeedMult = 2f;
+    
     public float gravity = 9.8f;
     public float jumpHeight = 5f;
+    
     public Transform groundCheck;
     public float grndCheckRadius = .4f;
     public LayerMask groundMask;
     
     private CharacterController _controller;
     private float _vSpeed;
+    private bool _onGround;
+    private bool _stopSprintOnLanding;
+    private bool _isSprinting;
     
     void Start()
     {
@@ -19,29 +26,79 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
+        _onGround = Physics.CheckSphere(groundCheck.position, grndCheckRadius, groundMask);
+        
+        MoveVertically();
+        MoveHorizontally();
+    }
+
+    void MoveHorizontally()
+    {
+        Sprint();
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
+
         Vector3 moveDir = (transform.right * x) + (transform.forward * z);
-        Vector3 hMov = moveDir * Time.deltaTime * speed;
+        Vector3 hMov = moveDir * (Time.deltaTime * speed);
 
         _controller.Move(hMov);
-        
-        if (Physics.CheckSphere(groundCheck.position, grndCheckRadius, groundMask))
+    }
+
+    void MoveVertically()
+    {
+        if (_onGround)
         {
-            if (_vSpeed <= 0f)
-            {
-                _vSpeed = -2f;
-            }
-            
+            //Debug.Log("on ground");
+            _vSpeed = Mathf.Max(0f, _vSpeed);
+
             if (Input.GetButtonDown("Jump"))
             {
                 _vSpeed = Mathf.Sqrt(jumpHeight * 2f * gravity);
             }
         }
-
-        _vSpeed -= gravity * Time.deltaTime;
+        else
+        {
+            //Debug.Log("not on ground");
+            _vSpeed -= gravity * Time.deltaTime;
+        }
 
         _controller.Move(new Vector3(0f, _vSpeed, 0f) * Time.deltaTime);
+    }
+
+    void Sprint()
+    {
+        if (!_stopSprintOnLanding)
+        {
+            if (Input.GetButtonDown("Sprint") && _onGround)
+            {
+                speed *= sprintSpeedMult;
+                _isSprinting = true;
+            }
+
+            if (Input.GetButtonUp("Sprint"))
+            {
+                if (_onGround && _isSprinting)
+                {
+                    speed /= sprintSpeedMult;
+                    _isSprinting = false;
+                }
+                else if (_isSprinting)
+                {
+                    _stopSprintOnLanding = true;
+                }
+            }
+        }
+        else if (_onGround)
+        {
+            speed /= sprintSpeedMult;
+            _isSprinting = false;
+            _stopSprintOnLanding = false;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("DIO PORCO");
     }
 }
